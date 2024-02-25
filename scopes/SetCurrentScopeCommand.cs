f3441f242a6c3f4231a1893c885dbaae2d5e80d2
Kirill Gubkin <kgubkin@list.ru>
2024-02-25 16:33:25 +0600

    first commit

---
+++ scopes/SetCurrentScopeCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace AppServer.Scopes
+{
+    public class SetCurrentScopeCommand : ICommand
+    {
+        object _scope;
+
+        public SetCurrentScopeCommand(object scope)
+        {
+            _scope = scope;
+        }
+
+        public void Execute()
+        {
+            InitCommand.currentScopes.Value = _scope;
+        }
+    }
+}
